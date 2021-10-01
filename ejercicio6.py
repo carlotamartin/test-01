c966c578cc5361beaeceff72752e55f21f7f9702
carlotamartin <91574355+carlotamartin@users.noreply.github.com>
2021-10-01 16:51:11 +0200

    cambio ej 6

--- ejercicio6.py
+++ ejercicio6.py
@@ -1,6 +1,5 @@
 age=0
 if age < 17:
- print("You are NOT old enough to drive")
+    print("You are NOT old enough to drive")
 else:
- print("You are old enough to drive")
- 
+    print("You are old enough to drive")
