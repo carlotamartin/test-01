8b02279e51f4faa9c18dabd460a202f67c745725
carlotamartin <91574355+carlotamartin@users.noreply.github.com>
2021-10-01 16:50:01 +0200

    Cambios de ejercicios 5 y 6

---
+++ ejercicio5.py
@@ -0,0 +1,11 @@
+a = 1
+if a == 1 or a > 10:
+  print("a is either 1 or above 10")
+name = "bob"
+hungry = True
+if name == "bob" and hungry == True:
+  print("bob is hungry")
+else if name == "bob" and not hungry:
+    print("Bob is not hungry")
+else: # If all other if conditions are not met
+    print("Not sure who this is or if they are hungry")
---
+++ ejercicio6.py
@@ -0,0 +1,6 @@
+age=0
+if age < 17:
+ print("You are NOT old enough to drive")
+else:
+ print("You are old enough to drive")
+ 
