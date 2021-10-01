8598ad3620ac3ddfe409e9be9714b3b68c27ac70
carlotamartin <91574355+carlotamartin@users.noreply.github.com>
2021-10-01 16:54:17 +0200

    ej 7

---
+++ ejercicio7.py
@@ -0,0 +1,10 @@
+i = 1
+while i <= 10:
+    print(i)
+    i = i + 1
+
+    websites = ["facebook.com", "google.com", "amazon.com"]
+for site in websites:
+    print(site)
+for i in range(5):
+    print(i)
